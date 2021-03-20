66678db276a6c7019fb0a44a1ab1a7741a314c69
Darren <258700581@qq.com>
2021-03-20 18:46:22 +0800

    change structure

---
+++ src/learn_type.rs
@@ -0,0 +1,18 @@
+pub fn learn_type() {
+    let mut a: i32 = 32;
+    println!("before hide a ={}", a);
+    a = 3;
+    println!("overwritten a={}", a);
+    let arr: [i32; 3] = [1, 2, 3];
+    for elem in arr.iter() {
+        println!("{}", elem);
+    }
+    print_arr(arr);
+}
+
+fn print_arr(arr: [i32; 3]) {
+    println!("----------");
+    for elem in &arr {
+        println!("{}", elem);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
+mod learn_type;
 fn main() {
-    let mut a: i32 = 32;
-    println!("before hide a ={}", a);
-
-    a = 3;
-    println!("overwritten a={}", a);
-
-    let arr: [i32; 3] = [1, 2, 3];
-    for elem in arr.iter() {
-        println!("{}", elem);
-    }
-    print_arr(arr);
-}
-
-fn print_arr(arr: [i32; 3]) {
-    println!("----------");
-    for elem in &arr {
-        println!("{}", elem);
-    }
+    learn_type::learn_type();
+    println!("123");
+    let s = 3;
+    println!("{}",s);
 }
